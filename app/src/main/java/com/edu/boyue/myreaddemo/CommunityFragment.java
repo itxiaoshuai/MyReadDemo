cdbabdfceeb612161027643ef1aab47d2e7ba0a2
罗帅 <luoshuai@boyue.com>
2017-10-13 10:18:48 +0800

    view +fragment初始化 tab

---
+++ app/src/main/java/com/edu/boyue/myreaddemo/CommunityFragment.java
@@ -0,0 +1,31 @@
+package com.edu.boyue.myreaddemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by arvin on 2017/10/13.
+ */
+
+public class CommunityFragment extends android.support.v4.app.Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater
+                .inflate(R.layout.fragment_community, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
--- app/src/main/java/com/edu/boyue/myreaddemo/MainActivity.java
+++ app/src/main/java/com/edu/boyue/myreaddemo/MainActivity.java
@@ -1,13 +1,54 @@
 package com.edu.boyue.myreaddemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @InjectView(R.id.viewpager)
+    ViewPager mViewpager;
+    private List<Fragment> mListFragment = new ArrayList<Fragment>();
+    private FragmentPagerAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        initFragment();
+        initView();
+        initData();
+    }
+
+    private void initFragment() {
+        mListFragment.add(new RecommendFragment());
+        mListFragment.add(new CommunityFragment());
+        mListFragment.add(new FindFragment());
+        mAdapter=new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mListFragment.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mListFragment.size();
+            }
+        };
+        mViewpager.setAdapter(mAdapter);
+    }
+
+    private void initData() {
+    }
+
+    private void initView() {
     }
 }
